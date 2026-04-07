0d9da5d3fa946ec57dcf25bbfb4ecce59e440a17
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2026-04-07 08:07:17 +0700

    ccommit for create new branch

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,31 @@ Route::get('/tenants', [TenantController::class, 'index']);
 Route::get('/leases', [LeaseController::class, 'index']);
 Route::get('/payments', [PaymentController::class, 'index']);
 Route::get('/maintenances', [MaintenanceController::class, 'index']);
+
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+});
+
+Route::get('/lease', function(){
+    return view('lease.index');
+});
+
+Route::get('/payment', function(){
+    return view('payment.index');
+});
+
+Route::get('/property', function(){
+    return view('property.index');
+});
+
+Route::get('/tenant', function(){
+    return view('tenant.index');
+});
+
+Route::get('/maintenance', function(){
+    return view('maintenance.index');
+});
+
+Route::get('/profile', function(){
+    return view('profile.index');
+});

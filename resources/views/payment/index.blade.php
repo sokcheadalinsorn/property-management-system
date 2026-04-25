1ce3833a102648dcd93082d51b34283d73b86765
nita <chheunnita169@gmail.com>
2026-04-25 09:07:41 +0700

    change index

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>payments</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
 </head>
+
 <body>
-    <x-sidebar />
+
+    <div class="flex">
+        <x-sidebar />
+        <h1>dsifjisa</h1>
+    </div>
+
 </body>
+
 </html>
